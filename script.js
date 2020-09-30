52ed7bfe7e23fb1a1eeaa7762527ba7d44de952e
Sebastian Graf <sgraf1337@gmail.com>
2020-09-30 23:30:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ﻿
 
-var basePath = "http://fixpt.de/sinfonieorchester.kit.edu/";
+var basePath = "http://sgraf812.github.io/sinfonieorchester.kit.edu/";
 
 //$('head').append('<link rel="stylesheet" href="' + basePath + 'style.css" type="text/css" />');
 
